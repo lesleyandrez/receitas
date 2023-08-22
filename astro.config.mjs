e0c39848ad97ab18f5b1cfb08be78ba84ba0a13e
Lesley Andreza <lesley.andrez@gmail.com>
2023-08-21 22:50:05 -0300

    💄 Ajustes visuais

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lesleyandrez.github.io',
     base: '/receitas',
-    integrations: [image({
-        serviceEntryPoint: '@astrojs/image/sharp'
-    })],
+    experimental: {
+        assets: true
+    },
 });
---
+++ src/data/receita.js
@@ -0,0 +1,36 @@
+export const receita = {
+    "Titulo": "Cookie Integral com Gotas de Chocolate",
+    "Introducao": "<p>Uma receita de cookies com farinha de trigo integral e com gotas de chocolate que você pode experimentar!</p>",
+    "Imagens": [
+        "01.webp",
+        "02.webp",
+        "03.jpeg",
+        "04.jpeg",
+        "05.webp",
+        "06.jpeg"
+    ],
+    "Ingredientes": [
+        "1 xícara de farinha de trigo integral",
+        "1/2 colher de chá de bicarbonato de sódio",
+        "1/4 colher de chá de sal",
+        "1/2 xícara de manteiga sem sal, em temperatura ambiente",
+        "1/2 xícara de açúcar mascavo",
+        "1/4 xícara de açúcar granulado",
+        "1 ovo grande",
+        "1 colher de chá de essência de baunilha",
+        "3/4 xícara de gotas de chocolate meio amargo"
+    ],
+    "Instrucoes": [
+        "Pré-aqueça o forno a 180°C e forre uma assadeira com papel manteiga.",
+        "Em uma tigela, misture a farinha de trigo integral, o bicarbonato de sódio e o sal.",
+        "Na tigela de uma batedeira, bata a manteiga até ficar cremosa. Adicione o açúcar mascavo e o açúcar granulado e bata até obter uma mistura leve e fofa.",
+        "Adicione o ovo e a essência de baunilha à mistura de manteiga e misture bem.",
+        "Adicione a mistura de farinha à mistura de manteiga e misture até ficar homogêneo.",
+        "Dobre as gotas de chocolate na massa com uma colher de pau ou espátula.",
+        "Faça bolas de massa com cerca de 2 colheres de sopa de tamanho e coloque na assadeira preparada, deixando espaço suficiente entre elas.",
+        "Asse por 12-15 minutos, até que as bordas estejam levemente douradas.",
+        "Deixe esfriar na assadeira por alguns minutos antes de transferir para uma grade para esfriar completamente.",
+        "Sirva e aproveite!"
+    ],
+    "Encerramento": "<p>Essa receita faz cerca de 16 cookies de tamanho médio. Você pode dobrar a receita se quiser fazer mais. Bom apetite!</p>"
+}
